4b28487ceaf4d2805bf4a14804148e16c7f00bb7
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-07-19 13:22:40 -0300

    add limit de caractere

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -85,5 +85,4 @@ export default {
 
     return info;
   }
-
 }
--- src/components/FeaturedMovie/FeatureMovie.js
+++ src/components/FeaturedMovie/FeatureMovie.js
@@ -12,6 +12,11 @@ function FeatureMovie({ item }) {
     genres.push(item.genres[i].name)
   }
 
+  let description = item.overview;
+  if(description.length > 200){
+    description = description.substring(0, 200)+'...'
+  }
+
   return (
     <section
       className="featured"
@@ -32,7 +37,7 @@ function FeatureMovie({ item }) {
               {item.number_of_seasons <= 1 ? "" : "s"}
             </div>
             <div className="featured--description">
-              {item.overview}
+              {description}
             </div>
             <div className="featured--buttons">
               <a href={`/watch/${item.id}`} className="featured--watchbutton"> Assistir</a>
